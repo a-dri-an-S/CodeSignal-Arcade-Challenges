a82d1a6b149c0cd89967d1203e543983516cc44d
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-02-27 10:28:58 -0800

    added boxBlur algo

--- Quizzes 19-24/22. avoidObstacles.js
+++ Quizzes 19-24/22. avoidObstacles.js
@@ -1,11 +1,10 @@
 // 22. You are given an array of integers representing coordinates of obstacles situated on a straight line. Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer. Find the minimal length of the jump enough to avoid all the obstacles.
 
 function avoidObstacles(inputArray) {
-    var max =Math.max(...inputArray);
-    for (var i = 1; i< max; i++) {
-        var divs = inputArray.some(x=>x%i==0)
-        if(!divs) return i;
+    let max = Math.max(...inputArray);
+    for (let i = 1; i < max; i++) {
+        let divs = inputArray.some(x => x % i === 0); 
+        if (!divs) return i;
     }
     return max +1;
-
 }
