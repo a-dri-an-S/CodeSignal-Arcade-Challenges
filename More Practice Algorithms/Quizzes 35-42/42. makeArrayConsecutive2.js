2e0858db021c31778acf80a0f77cf096704cc205
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-03-18 00:04:28 -0700

    makeArrayConsecutive2

---
+++ More Practice Algorithms/Quizzes 35-42/42. makeArrayConsecutive2.js
@@ -0,0 +1,8 @@
+// 42. Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+function makeArrayConsecutive2(statues) {
+    let min = Math.min(...statues);
+    let max = Math.max(...statues);
+    let length = statues.length - 1;
+    return max - (min + length);
+}
