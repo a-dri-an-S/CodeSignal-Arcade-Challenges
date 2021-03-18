86c403b8cd56cfa51369657ad1a31a3b2e39c7c7
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-03-17 22:35:20 -0700

    added arrayReplae algo

--- More Practice Algorithms/Quizzes 35-42/35. createArray.js
+++ More Practice Algorithms/Quizzes 35-42/35. createArray.js
@@ -1,5 +1,4 @@
 // Given an integer size, return array of length size filled with 1s.
 
-function createArray(size) {
-    return new Array(size).fill(1);
-}
+const createArray = size => new Array(size).fill(1);
+
---
+++ More Practice Algorithms/Quizzes 35-42/36. arrayReplace.js
@@ -0,0 +1,10 @@
+// 36. Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+    for(let i = 0; i < inputArray.length; i++){
+        if (inputArray[i] === elemToReplace){
+            inputArray[i] = substitutionElem;
+        }
+    }
+    return inputArray;
+}
