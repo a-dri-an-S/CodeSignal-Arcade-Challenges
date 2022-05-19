08826eab6689fa449a4fb91b8e9fde74eeca1b2a
Adrian S <adrian.salinas.1986@gmail.com>
2022-05-19 12:41:38 -0600

    evenDigitsOnly - Two pointer solution

--- Practice Algorithms/Quizzes 25-29/26. evenDigitsOnly.js
+++ Practice Algorithms/Quizzes 25-29/26. evenDigitsOnly.js
@@ -10,3 +10,31 @@ function evenDigitsOnly(n) {
     }
     return true
 }
+
+// TWO POINTER SOLUTION
+function solution(n) {
+    n = n.toString();
+    let j = n.length - 1;
+    
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] % 2 !== 0) return false;
+        if (n[j] % 2 !== 0) return false;
+        j--;
+        if (i === j) break;
+    }
+    
+    return true
+}
+
+// 1. Set n equal to n converted to string
+// - Use toString() method to convert integer to string
+// 2. Declare j and set it to length of n - 1
+//  - Subtract 1 because for 0-indexed array
+// 3. Use a for loop to iterate through n string
+// 4. If char at index-1 divided by 2 has a remainder of 0, return false
+//  - Use the modulo operator to check for remainder
+// 5. If char at index-j divided by 2 has a remainder of 0, return false
+//  - Use the modulo operator to check for remainder
+// 6. Decrement j by 1
+// 7. If i IS EQUAL TO j, break loop
+// 8. If no conditions are met and loop ends/breaks, return true
